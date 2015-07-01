83b295e5c773a25938d13d3c00a1d58e20e16ab4
Aciden <Aciden@Aciden-PC>
2015-07-01 23:13:36 +0300

    Clean code

--- db/Connect.php
+++ db/Connect.php
@@ -1,9 +1,4 @@
 <?php
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 namespace sptmFrm\db;
 
@@ -14,5 +9,5 @@ namespace sptmFrm\db;
  */
 class Connect
 {
-    //put your code here
+    //put your code here 
 }
